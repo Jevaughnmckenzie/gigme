697f43057654b9abbe80cc297b742f9bbe9d27eb
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-07-09 16:56:11 +0000

    added a show_details method to the cli, made the show_gigs method more robust by allowing it to go back to previous menus.

--- NOTES.md
+++ NOTES.md
@@ -1,10 +1,10 @@
 GIGME CLI
 
 1. User is greeted by Gigme New York
-2. user gets a list of New York loactions
+2. user gets a list of New York locations
 3. user is asked to input a location
-4. user gets a numbered list of gig catagories
-5. user selects a catagory by number
-6. user gets a list of 10 gigs in that area under that catagory
+4. user gets a numbered list of gig categories
+5. user selects a category by number
+6. user gets a list of 10 gigs in that area under that category
 7. user can select the number of that gig to get more info
-8. user can go back to results list, access the next ten gigs (up to 50), go back to gig catagories, or exit
+8. user can go back to results list, access the next ten gigs (up to 50), go back to gig categories, or exit
--- lib/gigme/cli.rb
+++ lib/gigme/cli.rb
@@ -2,10 +2,6 @@ class Gigme::CLI
   def call
     puts "Welcome to Gigme - New York!"
     show_locations
-    ask_for_location
-    show_gig_catagories
-    ask_for_gig_catagory
-    show_gigs
   end
 
   def show_locations
@@ -19,15 +15,17 @@ class Gigme::CLI
       6. Long Island
       7. Westchester
     DOC
+    ask_for_location
   end
 
   def ask_for_location
     puts
     puts "Where would you like to search for gigs?"
     input = gets.strip.to_i
+    show_gig_catagories(input)
   end
 
-  def show_gig_catagories
+  def show_gig_catagories(input)
     puts
     puts "Here's list of gig catagories:"
     puts(<<-DOC.sub(/\n$/, ''))
@@ -40,15 +38,49 @@ class Gigme::CLI
       7. Talent
       8. Writing
     DOC
+    ask_for_gig_catagory
   end
 
   def ask_for_gig_catagory
     puts
     puts "What kind of gig are you looking for?"
     input = gets.strip.to_i
+    show_gigs(input)
   end
 
-  def show_gigs
-    puts "Here's the most recent gigs according to your preferences:"
+  def show_gigs(input)
+    puts "Here's the most recent gigs according to your preferences"
+    puts "Select the gig's number to get more details, \ntype 'categories' to choose from other gig catagories,\ntype 'locations' to select a new location,\nor type 'exit' to quit."
+    puts(<<-DOC.sub(/\n$/, ''))
+      1. gig
+      2. gig
+      3. gig
+      4. gig
+      5. gig
+      6. gig
+      7. gig
+      8. gig
+    DOC
+
+    gig_input = gets.strip.downcase
+
+    if gig_input = 'categories'
+      show_gig_catagories
+    elsif gig_input = 'locations'
+      show_locations
+    elsif gig_input = 'exit'
+      exit
+    elsif gig_input.to_i > 0
+      show_detail(input.to_i - 1)
+    else
+      puts "Not sure what you meant. Please choose from our list of gigs or enter 'locations', 'catagories', or 'exit'."
+      show_gigs(input)
+    end
+  end
+
+  def show_detail
+    puts "..........."
+    puts "gig details"
+    puts "..........."
   end
 end
