334e1a839c809a196e6862ff59b8801802fcd3b1
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-07-11 20:11:05 +0000

    scraped gig title and compensation from gig details page in Gigme::Scraper_show_gig_details

--- lib/gigme/scraper.rb
+++ lib/gigme/scraper.rb
@@ -69,7 +69,12 @@ class Gigme::Scraper
 
     # test code
     gig_details_page = Nokogiri::HTML(open(BASE_PATH + "/brk/cpg/6214759555.html"))
-    # binding.pry
+
+    # *********** gig title code ***********
+
+    gig_title = gig_details_page.css(".postingtitletext #titletextonly").text
+
+    # *********** gig description code ***********
     gig_description = gig_details_page.css("#postingbody").text
     # Remove any irrelavant text
     gig_description_text_array = gig_description.split(/\n/)
@@ -77,5 +82,17 @@ class Gigme::Scraper
 
     relevant_gig_text = gig_description_text_array[(end_of_irrelevant_text + 1)..(gig_description_text_array.count-1)].join
     final_gig_description = relevant_gig_text.strip
+
+    # *********** gig compensation code ***********
+
+    compensation = gig_details_page.css(".attrgroup span").text
+
+    puts "compensation"
+
+    puts gig_title
+    puts
+    puts final_gig_description
+    puts
+    puts compensation
   end
 end
