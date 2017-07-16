24a6201bd38bf3976393be956b144e3d266ddf1f
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-07-16 22:17:38 +0000

    create CategoryLoader class inside category_loader.rb

---
+++ lib/gigme/category_loader.rb
@@ -0,0 +1,23 @@
+class CategoryLoader < Loader
+
+  attr_accessor :location
+
+  def initialize(location)
+    @location = location
+  end
+
+  def load
+    # ["computer", "labor", "creative", "events"]
+    location_page = super(location[:href])
+    gigs_results_html = location_page.css(".jobs div#ggg ul a")
+
+    gigs_results_html.map do |gig_category|
+      {
+        name: gig_category.children.text,
+        # href:
+      }
+
+    end
+  end
+
+end
