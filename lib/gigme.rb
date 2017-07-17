d4e02152105693d60a621d972793c2a24f89e9be
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-07-17 17:29:48 +0000

    update lib/gigme.rb so that it requires all of the files with the Loader classes

--- lib/gigme.rb
+++ lib/gigme.rb
@@ -3,8 +3,14 @@ require "nokogiri"
 require "pry"
 
 require_relative "gigme/version"
-require_relative "gigme/cli"
+require_relative "gigme/loader"
 require_relative "gigme/scraper"
+require_relative "gigme/category_loader"
+require_relative "gigme/gig_loader"
+require_relative "gigme/gig"
+require_relative "gigme/gig_results_loader"
+require_relative "gigme/cli"
+require_relative "gigme/location_loader"
 
 module Gigme
   # Your code goes here...
